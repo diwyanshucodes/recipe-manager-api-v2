9ad77559bb6c6bd630297bd3efcdcf07328f251d
diwyanshucodes <diwyanshu.codes@gmail.com>
2026-06-17 20:03:22 +0530

    post install for prisma migrations, port from env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import app from "./app"
-
-app.listen(3000, ()=>{
-    console.log('Server running on port: 3000')
+const port = Number(process.env.PORT) || 3000;
+app.listen(port, ()=>{
+    console.log(`Server running on port: ${port}`)
 })
